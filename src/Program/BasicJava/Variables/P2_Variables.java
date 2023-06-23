d66acda2eabbc66e3e4e79e5e727aaba3190b84b
Avval <yadavavval@gmail.com>
2023-06-23 15:05:34 +0530

    Program 2

---
+++ src/Program/BasicJava/Variables/P2_Variables.java
@@ -0,0 +1,20 @@
+package Program.BasicJava.Variables;
+
+public class P2_Variables {
+
+    int a = 10; // Global Variable
+
+    static int b = 20; // Static Variable
+
+    public static void main(String[] args) {
+
+        int c = 30; // Instance Variable
+
+        methods();
+    }
+
+    public static void methods() {
+
+        int d = 40; // Local Variable
+    }
+}
